ddcadb74260f3fe7f1a9fb1c1ebe60ba8a637c26
Johnny Stene <colenthri576@gmail.com>
2017-11-30 19:33:25 -0700

    Updated bot.js to include basic database functions and updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GameBot
 Discord bot with a game.
+WARNING: This app is in a *very* early alpha stage.
+It is most likely unstable and should NOT BE USED.
+Have a nice day.
+
+## Setup
+1. Install MongoDB from mongodb.com
+2. Use `pip` to install `discord.js`, `mongodb` and `fs`
+3. Update the values in `config.json` to match your desired settings.
--- bot.js
+++ bot.js
@@ -5,38 +5,125 @@
  * It is most likely unstable and should NOT BE USED.
  * Have a nice day.
  */
+ 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const fs = require('fs')
+const MongoClient = require('mongodb').MongoClient;
+const url = config.dburl;
+
+//Database functions
+function getUserData(uid)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var query = { id: uid };
+		db.collection("users").find(query).toArray(function(err, result) {
+			if (err) throw err;
+			return(result);
+			db.close();
+		});
+	}); 
+}
+
+function changeUserHP(uid, nhp)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		userquery = { id: uid };
+		update = { hp: nhp };
+		db.collection("users").updateOne(userquery, update, function(err, res) {
+			if (err) throw err;
+			console.log("Changed ${uid}'s HP to ${nhp}.");
+			db.close();
+		});
+	}); 
+}
+
+function changeUserXP(uid, nxp)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		userquery = { id: uid };
+		update = { xp: nxp };
+		db.collection("users").updateOne(userquery, update, function(err, res) {
+			if (err) throw err;
+			console.log("Changed ${uid}'s XP to ${nxp}.");
+			db.close();
+		});
+	}); 
+}
+
+function addUser(uid)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var userdata = { id: uid, hp: 0, xp: 0, lvl: 0, gold: 0 };
+		db.collection("users").insertOne(userdata, function(err, res) {
+			if (err) throw err;
+			console.log("Added User ID ${uid} to users.");
+			db.close();
+		});
+	}); 
+}
+
+function changeUserLevel(uid, nlvl)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		userquery = { id: uid };
+		update = { lvl: nlvl };
+		db.collection("users").updateOne(userquery, update, function(err, res) {
+			if (err) throw err;
+			console.log("Changed ${uid}'s HP to ${nlvl}.");
+			db.close();
+		});
+	}); 
+}
+
+function changeUserGold(uid, ngold)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		userquery = { id: uid };
+		update = { gold: ngold };
+		db.collection("users").updateOne(userquery, update, function(err, res) {
+			if (err) throw err;
+			console.log("Changed ${uid}'s HP to ${ngold}.");
+			db.close();
+		});
+	}); 
+}
 
 client.on("ready", () => {
 	console.log("GameBot started.");
 });
 
 client.on("message", (message) => {
+	//Get the args
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 	
-	if (talkedRecently.has(message.author.id)) return;
-
-	// Adds the user to the set so that they can't talk for 2.5 seconds
-	talkedRecently.add(message.author.id);
+	//Get user data from database
+	var user = getUserData(message.author.id);
 	
-	setTimeout(() => {
-		talkedRecently.delete(message.author.id);
-	}, 2500);
-	
+	//Commands
 	if(command === "ping")
 	{
 		message.channel.sendMessage("YO WASSUP");
 	}
+	
 });
 
+//Run when someone joins
 client.on("guildMemberAdd", (member) => {
   console.log(`New user "${member.user.username}" has joined "${member.guild.name}"` );
   member.guild.channels.get("welcome").send(`"Welcome, ${member.user.username}!"`);
+  addUser(member.user.id);
 });
 
+//Error printing
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 

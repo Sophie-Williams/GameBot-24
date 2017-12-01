1122f82d4d821ca3db9f69918551cde4d2c7a4c8
Johnny Stene <colenthri576@gmail.com>
2017-12-01 12:43:28 -0700

    Fixed crashing bugs and database errors. You can now add and read user data, and delete all of it. First working-ish prototype.

--- bot.js
+++ bot.js
@@ -14,16 +14,26 @@ const MongoClient = require('mongodb').MongoClient;
 const url = config.dburl;
 const client = new Discord.Client();
 let prefix = config.prefix;
+var secret = 0;
+
+//Secret generation for things.
+function generateSecret()
+{
+	secret = Math.floor((Math.random() * 1000000000) + 10000000);
+}
 
 //Database functions that might work?
-function getUserData(uid)
+function getUserData(userid)
 {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var query = { id: uid };
+		q = "{ \"uid\": \"" + userid + "\" }";
+		var query = JSON.parse(q);
 		db.collection("users").find(query).toArray(function(err, result) {
 			if (err) throw err;
-			return(result);
+			if(result[0] == undefined) console.log("User " + userid + "does not exist.");
+			else console.log(result[0]);
+			return(result[0]);
 			db.close();
 		});
 	});
@@ -61,10 +71,10 @@ function addUser(uid)
 {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var userdata = { id: uid, hp: 0, xp: 0, lvl: 0, gold: 0 };
+		var userdata1 = "{ \"uid\": \"" + uid + "\", \"hp\": 0, \"xp\": 0, \"lvl\": 0, \"gold\": 0 }";
+		var userdata = JSON.parse(userdata1);
 		db.collection("users").insertOne(userdata, function(err, res) {
 			if (err) throw err;
-			console.log("Added User ID ${uid} to users.");
 			db.close();
 		});
 	});
@@ -78,7 +88,7 @@ function changeUserLevel(uid, nlvl)
 		update = { lvl: nlvl };
 		db.collection("users").updateOne(userquery, update, function(err, res) {
 			if (err) throw err;
-			console.log("Changed ${uid}'s HP to ${nlvl}.");
+			console.log("Changed ${uid}'s level to ${nlvl}.");
 			db.close();
 		});
 	});
@@ -92,7 +102,7 @@ function changeUserGold(uid, ngold)
 		update = { gold: ngold };
 		db.collection("users").updateOne(userquery, update, function(err, res) {
 			if (err) throw err;
-			console.log("Changed ${uid}'s HP to ${ngold}.");
+			console.log("Changed ${uid}'s gold to ${ngold}.");
 			db.close();
 		});
 	});
@@ -109,19 +119,76 @@ client.on("message", (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
-	//Get user data from database
-	var user = getUserData(message.author.id);
-
 	//Commands
 	if(command === "ping")
 	{
-		message.channel.send("YO WASSUP");
+		message.channel.send("Pong!");
 	}
-
-	if(command == "testAddUser")
+	else if(command === "adduser")
 	{
-		client.emit("guildMemberAdd", args[0]); //pretend to add a user
-		message.channel.send(getUserData(args[0])); //send user data for the fake user
+		console.log("Adding user: " + args[0]); //add user
+		try
+		{
+			addUser(args[0]);
+			message.channel.send("Added user " + args[0] + " to database.");
+			console.log("Successfully added " + args[0]);
+		}
+		catch(e)
+		{
+			message.channel.send("Error adding " + args[0] + "to database. Check the log for more info.");
+			console.log(e);
+		}
+	}
+	else if(command === "getdata")
+	{
+		try
+		{
+			message.channel.send("Stored data for " + args[0] + " will show up in the log.");
+			console.log("User data for " + args[0]);
+			udata = getUserData(args[0]);
+		}
+		catch (e)
+		{
+			message.channel.send("Error getting user data. Check the log for more info.");
+			console.log("Error getting user data.");
+			console.log(e);
+		}
+	}
+	else if(command === "wipeuserdata")
+	{
+		if(secret == 0)
+		{
+			message.channel.send("Are you sure? This will wipe all user data from the database and every user will need to be added manually.");
+			message.channel.send("If you are 100% sure, run this command with the secret that was just added to the log.");
+			generateSecret();
+			console.log("Are you sure? Secret is " + secret);
+		}
+		else if(args[0] == secret)
+		{
+			message.channel.send("Deleting all user data.");
+			try
+			{
+				MongoClient.connect(url, function(err, db) {
+	  			if (err) throw err;
+	  			db.collection("users").drop(function(err, delOK) {
+	    			if (err) throw err;
+	    			if (delOK) console.log("All user data removed.");
+	    			db.close();
+	  			});
+				});
+			}
+			catch(e)
+			{
+				message.channel.send("Error while deleting user data. Check the log for more info.")
+				console.log(e);
+			}
+			secret = 0;
+		}
+		else
+		{
+			message.channel.send("Invalid secret. Generating a new one.");
+			generateSecret();
+		}
 	}
 });
 
@@ -129,7 +196,6 @@ client.on("message", (message) => {
 client.on("guildMemberAdd", (member) => {
   console.log(`New user "${member.user.username}" has joined "${member.guild.name}"` );
   member.guild.channels.get("welcome").send(`"Welcome, ${member.user.username}!"`);
-  addUser(member.user.id);
 });
 
 //Error printing and stuff
